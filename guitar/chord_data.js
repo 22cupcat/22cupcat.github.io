7fddba29937a7d8009066fe22dbb0bae7af657a6
Ryan <22cupcat@gmail.com>
2016-04-29 17:57:48 +0800

    add guitar page

---
+++ guitar/chord_data.js
@@ -0,0 +1,43 @@
+var chordMap = new Map();
+
+chordMap.set('Amaj', ['Amaj', [1,1,1,1,1,0], [[],[42],[32],[22],[],[]]]);
+chordMap.set('Am', ['Am', [1,1,1,1,1,0], [[],[21],[42],[32],[],[]]]);
+chordMap.set('Amaj7', ['Amaj7', [1,1,1,1,1,0], [[],[31],[42],[22],[],[]]]);
+chordMap.set('A7', ['A7', [1,1,1,1,1,0], [[],[42],[],[22],[],[]]]);
+chordMap.set('Am7', ['Am7', [1,1,1,1,1,0], [[],[21],[42],[],[],[]]]);
+
+chordMap.set('Bmaj', ['koko', 'Bmaj']);
+chordMap.set('Bm', ['Bm', [0,1,1,1,1,0], [[],[52],[23],[44],[34],[]]]);
+chordMap.set('Bmaj7', ['koko', 'Bmaj7']);
+chordMap.set('B7', ['B7', [1,1,1,1,1,0], [[],[41],[52],[32],[12],[]]]);
+chordMap.set('Bm7', ['koko', 'Bm7']);
+
+chordMap.set('Cmaj', ['Cmaj', [1,1,1,1,1,0], [[],[21],[42],[53],[],[]]]);
+chordMap.set('Cm', ['koko', 'Cm']);
+chordMap.set('Cmaj7', ['Cmaj7', [1,1,1,1,1,0], [[],[],[42],[53],[],[]]]);
+chordMap.set('C7', ['C7', [1,1,1,1,1,0], [[],[21],[42],[53],[33],[]]]);
+chordMap.set('Cm7', ['koko', 'Cm7']);
+
+chordMap.set('Dmaj', ['Dmaj', [1,1,1,1,0,0], [[],[32],[12],[23],[],[]]]);
+chordMap.set('Dm', ['Dm', [1,1,1,1,0,0], [[],[11],[32],[23],[],[]]]);
+chordMap.set('Dmaj7', ['Dmaj7', [1,1,1,1,0,0], [[],[32],[22],[12],[],[]]]);
+chordMap.set('D7', ['D7', [1,1,1,1,0,0], [[],[21],[32],[12],[],[]]]);
+chordMap.set('Dm7', ['Dm7', [1,1,1,1,0,0], [[],[11,21],[32],[],[],[]]]);
+
+chordMap.set('Emaj', ['Emaj', [1,1,1,1,1,1], [[],[31],[52],[42],[],[]]]);
+chordMap.set('Em', ['Em', [1,1,1,1,1,1], [[],[],[52],[42],[],[]]]);
+chordMap.set('Emaj7', ['koko', 'Emaj7']);
+chordMap.set('E7', ['E7', [1,1,1,1,1,1], [[],[31],[52],[],[],[]]]);
+chordMap.set('Em7', ['Em7', [1,1,1,1,1,1], [[],[],[52],[],[],[]]]);
+
+chordMap.set('Fmaj', ['Fmaj', [1,1,1,1,0,0], [[],[11,21],[32],[43],[],[]]]);
+chordMap.set('Fm', ['koko', 'Fm']);
+chordMap.set('Fmaj7', ['Fmaj7', [1,1,1,1,0,0], [[],[21],[32],[43],[],[]]]);
+chordMap.set('F7', ['koko', 'F7']);
+chordMap.set('Fm7', ['koko', 'Fm7']);
+
+chordMap.set('Gmaj', ['Gmaj', [1,1,1,1,1,1], [[],[52],[63],[13],[],[]]]);
+chordMap.set('Gm', ['koko', 'Gm']);
+chordMap.set('Gmaj7', ['koko', 'Gmaj7']);
+chordMap.set('G7', ['G7', [1,1,1,1,1,1], [[],[11],[52],[63],[],[]]]);
+chordMap.set('Gm7', ['koko', 'Gm7']);
